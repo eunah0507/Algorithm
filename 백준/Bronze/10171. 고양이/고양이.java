22ceef812ebbbcc1947a5714ef0bcb7c633233f0
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2023-07-15 19:32:08 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// 고양이 출력하기 
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
