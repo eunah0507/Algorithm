551849ceeafb2ac81006c8495803447a4ebff920
eunah0507 <eunah0507@naver.com>
2023-12-03 23:48:59 +0900

    12월 3일 문자+숫자 추출 문제

---
+++ 인프런 알고리즘/problem_11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class problem_11 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next()+" ";
+		int cnt = 1;
+		String answer = "";
+		
+		for (int i = 0; i < str.length()-1; i++) {
+			if (str.charAt(i) == str.charAt(i+1)) {
+				cnt++;
+			}else {
+				answer += str.charAt(i);
+				if (cnt>1) {
+					answer += String.valueOf(cnt);
+					cnt = 1;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+}
