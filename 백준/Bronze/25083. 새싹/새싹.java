4976593826858c06535de923b49fcc772399d8c7
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2023-12-07 15:29:47 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 문자 그대로 출력하기
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+}
