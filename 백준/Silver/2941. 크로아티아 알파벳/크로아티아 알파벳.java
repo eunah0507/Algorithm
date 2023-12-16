df597d01d0fdd74cd36c5a018ecf53fa35a2ee26
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2023-12-16 18:46:36 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -6,45 +6,19 @@ public class Main {
 		// contain()을 통해서 문자를 추출하고, 길이를 세는 문제
 		
 		// 1. 테스트케이스 String을 nextLine()을 받아온다.
-		// 2. if문을 돌려 contain()을 통해 해당 문자가 존재하는지 찾는다.
-		// 3. 맞으면 해당 문자를 다른 문자로 바꿔준다.
-		// 4. else if문을 돌리는 것이 아니라, 독립된 if를 계속 돌린다.
-		// 5. 모두 바꿔준 후, 길이를 세준다.
+		// 2. String을 배열로 만들고, 값을 직접 넣어준다.
+		// 3. for문을 돌린 후 if문을 돌려 값을 모두 a로 변환해준다.
+		// 4. str.length()를 출력한다.
 		
 		Scanner sc = new Scanner(System.in);
 		
 		String str = sc.nextLine();
+		String [] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		
-		if(str.contains("c=")) {
-			str = str.replace("c=", "a");
-		}
-		
-		if(str.contains("c-")) {
-			str = str.replace("c-", "a");
-		}
-		
-		if(str.contains("dz=")) {
-			str = str.replace("dz=", "a");
-		}
-		
-		if(str.contains("d-")) {
-			str = str.replace("d-", "a");
-		}
-		
-		if(str.contains("lj")) {
-			str = str.replace("lj", "a");
-		}
-		
-		if(str.contains("nj")) {
-			str = str.replace("nj", "a");
-		}
-		
-		if(str.contains("s=")) {
-			str = str.replace("s=", "a");
-		}
-		
-		if(str.contains("z=")) {
-			str = str.replace("z=", "a");
+		for (int i = 0; i < arr.length; i++) {
+			if (str.contains(arr[i])) {
+				str = str.replace(arr[i], "a");
+			}
 		}
 		
 		System.out.println(str.length());
