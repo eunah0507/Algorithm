07820780ed1be41a21ad3cd77c4debe3e4d71016
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2023-12-31 19:39:38 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 이차복잡도와 비슷한 시간복잡도 문제 풀기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = a * a * a;
+		
+		System.out.println(b);
+		System.out.println(3);
+		
+	
+		
+	}
+}
