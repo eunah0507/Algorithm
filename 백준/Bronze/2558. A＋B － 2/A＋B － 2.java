00a973b700e71c42de3b521418485351f706fa4a
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-01-01 23:02:58 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// A + B 문제
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int answer = a+b;
+		
+		System.out.println(answer);
+	}
+}
