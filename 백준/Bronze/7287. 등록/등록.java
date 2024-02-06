aa59fc20a35969e9c412495c78ccd714a31f8820
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-06 11:32:24 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 백준 스펙 출력하기
+
+        System.out.println("145");
+        System.out.println("eunah0507");
+
+    }
+}
