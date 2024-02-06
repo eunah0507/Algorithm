faa89b6f38cab1748167806c4ebcbb6a611a292f
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-06 10:37:52 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 출력 그대로 출력하기
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\");
+        sb.append("\n");
+        sb.append("    ( oo|");
+        sb.append("\n");
+        sb.append("    _\\=/_");
+        sb.append("\n");
+        sb.append("   /  _  \\");
+        sb.append("\n");
+        sb.append("  //|/.\\|\\\\");
+        sb.append("\n");
+        sb.append(" ||  \\ /  ||");
+        sb.append("\n");
+        sb.append("============");
+        sb.append("\n");
+        sb.append("|          |");
+        sb.append("\n");
+        sb.append("|          |");
+        sb.append("\n");
+        sb.append("|          |");
+
+        System.out.println(sb.toString());
+
+    }
+}
