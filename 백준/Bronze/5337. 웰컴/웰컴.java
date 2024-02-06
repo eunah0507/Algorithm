c00f80333f627cc7d68e7f7bf30ccaa11a606aa8
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-06 10:53:12 +0900

    [Bronze V] Title: 웰컴, Time: 112 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 그대로 출력하기
+
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
