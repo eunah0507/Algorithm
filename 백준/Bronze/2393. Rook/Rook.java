d999e4ffb7a45d5109ab682fa781d6ed31b91e0f
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-08 12:18:38 +0900

    [Bronze V] Title: Rook, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // 그대로 출력하기
+
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+
+
+
+    }
+}
