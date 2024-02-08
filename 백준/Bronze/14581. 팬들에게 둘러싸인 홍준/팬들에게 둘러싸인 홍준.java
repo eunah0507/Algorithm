9c2bdf9860b1b95e97fc86a1a45c4d8841574ba3
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-08 11:11:56 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18220 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // 입력받은대로 출력하기
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
