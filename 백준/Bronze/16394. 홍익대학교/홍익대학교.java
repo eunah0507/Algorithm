151043386bd88b8ee50af7556220f29bb00f8684
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-10 16:34:14 +0900

    [Bronze V] Title: 홍익대학교, Time: 200 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // 조건에 맞춰 출력하기
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int answer = num - 1946;
+
+        System.out.println(answer);
+    }
+}
