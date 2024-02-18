eb3bba0796f5622e60dd148b4e1864a04df143ff
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-18 20:16:09 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 140 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -1,17 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt();
-        if(a < b)
-            System.out.println("-1");
-        else{
-            int x = (a+b)/2, y = (a-b)/2;
-            if(x+y==a && x-y==b)
-                System.out.println(x+" "+y);
-            else
-                System.out.println("-1");
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+
+        // 지수가 원섭이한테 장난치는 문제..
+        // 지수가 장난쳐서 내가 대신 푸는....
+        // 두 팀 점수의 합과 차가 주어지므로 두 팀의 경기 결과를 구하는 문제
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int scoreSum = Integer.parseInt(st.nextToken());
+        int scoreMin = Integer.parseInt(st.nextToken());
+
+        if (scoreSum < scoreMin) {
+            sb.append(-1);
+        } else {
+            int scoreA = (scoreSum + scoreMin) / 2;
+            int scoreB = (scoreSum - scoreMin) / 2;
+
+            if (scoreA + scoreB == scoreSum && scoreA - scoreB == scoreMin) {
+                sb.append(scoreA).append(" " + scoreB);
+            } else {
+                sb.append(-1);
+            }
         }
+
+        System.out.println(sb.toString());
     }
 }
