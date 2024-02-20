a9cf3dab5d334330f0cacc55562fb3ffb7cdb56a
eunah0507 <eunah0507@naver.com>
2024-02-20 18:49:13 +0900

    정렬단계_선택정렬 문제

---
+++ 인프런 알고리즘/Sorting_01.java
@@ -0,0 +1,29 @@
+package Sorting_and_Searching;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Sorting_01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
