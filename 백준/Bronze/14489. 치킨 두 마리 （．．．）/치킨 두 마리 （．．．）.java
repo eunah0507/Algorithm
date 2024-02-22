a5dccf1a5b84e2191ac2a328016c7b36aedc709f
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-02-22 11:19:04 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 120 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int bankA = Integer.parseInt(st.nextToken());
+        int bankB = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int chichken = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        if ((bankA + bankB) >= chichken * 2){
+            answer = (bankA + bankB) - (chichken * 2);
+        }else{
+            answer = bankA + bankB;
+        }
+
+        sb.append(answer);
+        System.out.println(sb.toString());
+
+    }
+}
