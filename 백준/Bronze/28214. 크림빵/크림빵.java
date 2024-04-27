22075b0154a8514775635f0cf4643c6f9d22e2ff
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-04-28 01:07:19 +0900

    [Bronze III] Title: 크림빵, Time: 136 ms, Memory: 14444 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.java
+++ 백준/Bronze/28214. 크림빵/크림빵.java
@@ -1,48 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main extends Exception {
+public class Main {
     public static void main(String[] args) throws IOException {
-        // KOI빵집은 N*K개의 빵을 만들었다.
-        // 순서대로 K개씩 묶어서 한 묶음으로 판매할 것이고, N개의 묶음이 있다.
-        // 한 묶음에 크림이 없는 빵이 P개 이상이라면, 그 줄은 판매할 수 없다.
-        // 첫 째 줄에 N,K,P가 주어진다.
-        // 둘 째 줄에 크림 유뮤가 주어진다. 크림X = 0, 크림O = 1
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
-        int[] noCream = new int[N * K];
-        // 크림이 없는 빵의 갯수를 셀 변수
-        int count = 0;
 
+        int[] bread = new int[N * K];
         st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N * K; i++) {
+            bread[i] = Integer.parseInt(st.nextToken());
+        }
 
+        int result = N;
         for (int i = 0; i < N; i++) {
-            // 새로운 묶음 값을 받기 전, count를 초기화한다.
-            count = 0;
+            int creamless = 0;
             for (int j = 0; j < K; j++) {
-                // 새로운 묶음 단위의 값을 받는다.
-                noCream[j] = Integer.parseInt(st.nextToken());
-
-                if (noCream[j] == 0) {
-                    count++;
+                if (bread[i * K + j] == 0) {
+                    creamless++;
                 }
             }
-
-            // 크림이 없는 빵이 P개 이상 넘어가면 한 묶음을 뺀다.
-            if (count >= P) {
-                N--;
+            if (creamless >= P) {
+                result--;
             }
         }
 
-        sb.append(N);
-        System.out.println(sb.toString());
+        System.out.println(result);
     }
 }
