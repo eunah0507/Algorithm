0c3b12765b1b058bf159be7822fe113b399c04d3
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-04-30 21:36:18 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 232 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+        // 테이블이 W개씩 H행에 걸쳐 있을 때
+        // 세로로 N칸, 가로로 M칸 이상 비우고 앉아야 한다.
+        // 최대 몇 명 수용 가능한가?
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+    }
+}
