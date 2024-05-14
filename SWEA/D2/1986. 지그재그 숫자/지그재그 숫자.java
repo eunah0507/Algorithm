9f01fe193672e4cf72b3324a9b7ad68a10587406
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-05-14 20:51:29 +0900

    [D2] Title: 지그재그 숫자, Time: 113 ms, Memory: 26,044 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -1,91 +1,34 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
+import java.util.*;
+import java.io.*;
+
+
 class Solution
 {
-	public static void main(String args[]) throws Exception
+	public static void main(String args[]) throws IOException
 	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
+        // 홀수는 더하고 짝수는 더했을 때의 결과
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-	Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();//1
-		
-		
-		for(int test_case = 1; test_case <= T; test_case++) {//2
-			
-			
-			int a;//3
-			a=sc.nextInt();
-			
-			int b;
-			b=0;
-			
-			for(int i=1; i<=a; i++) {//3-1
-			
-			if(i%2==1) //4
-			{
-				b=b+i;
-			}else if(i%2==0) //5
-			{
-				b=b-i;
-			}
-			}
-			
-			System.out.println("#"+test_case+" "+b);
-		}
-		
-		
-		/*
-		 * 1. 테스트케이스를 받아온다.
-		 * 2. for문(돌리는 케이스 수만큼)안에서 작업을 한다. 
-		 * 3. 숫자 1개를 받아온다.
-		 * 3-1. 해당 숫자만큼 for문을 돌린다.
-		 * 4. 홀수일 때는 더한다
-		 * 5. 짝수일 때는 뺀다.
-		 * 6. 출력한다.
-		 * */
-		
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int score = 0;
+
+        for (int i = 1; i <= num; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            score = 1;
+
+            for (int j = 2; j <= number; j++) {
+                if (j % 2 == 0){
+                    score -= j;
+                }else{
+                    score += j;
+                }
+            }
+            sb.append("#").append(i).append(" ").append(score).append("\n");
+        }
+        System.out.println(sb.toString());
 	}
 }
