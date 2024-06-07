f824328f0102ed4a478d3ad8edd5ed98940a5423
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-06-07 23:36:32 +0900

    [Bronze III] Title: 수학적 호기심, Time: 404 ms, Memory: 25248 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int count = 0;
+
+            for (int a = 1; a < n; a++) {
+                for (int b = a + 1; b < n; b++) {
+                    if ((a * a + b * b + m) % (a * b) == 0) {
+                        count++;
+                    }
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
