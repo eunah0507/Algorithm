99c5c484dbd270b4fce4718f94a332e1f66a80dd
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-06-07 16:54:04 +0900

    [Bronze III] Title: A/B - 3, Time: 248 ms, Memory: 17432 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.java
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.java
@@ -14,13 +14,29 @@ public class Main extends Exception {
         BigInteger numA = new BigInteger(st.nextToken());
         BigInteger numB = new BigInteger(st.nextToken());
 
-        BigInteger answerA = numA.divide(numB);
-        BigInteger answerB = numA.remainder(numB);
+        // numA와 numB가 음,양수일 때를 나눠 출력한다.
+        if (numA.compareTo(BigInteger.ZERO) == -1 && numB.compareTo(BigInteger.ZERO) == -1){
+            BigInteger answerA = numA.divide(numB).add(BigInteger.ONE);
+            BigInteger answerB = numA.remainder(numB).subtract(numB);
 
-        sb.append(answerA).append("\n");
-        sb.append(answerB).append("\n");
+            sb.append(answerA).append("\n");
+            sb.append(answerB).append("\n");
+        }else if(numA.compareTo(BigInteger.ZERO) == -1 && numB.compareTo(BigInteger.ZERO) == 1){
+            BigInteger answerA = numA.divide(numB).subtract(BigInteger.ONE);
+            BigInteger answerB = numA.remainder(numB).add(numB);
+
+            sb.append(answerA).append("\n");
+            sb.append(answerB).append("\n");
+        }else{
+            BigInteger answerA = numA.divide(numB);
+            BigInteger answerB = numA.remainder(numB);
+
+            sb.append(answerA).append("\n");
+            sb.append(answerB).append("\n");
+        }
 
         System.out.println(sb.toString());
+
 
     }
 }
