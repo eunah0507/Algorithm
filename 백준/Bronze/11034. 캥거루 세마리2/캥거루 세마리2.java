d3c5a3167c460896cb691b5ecff5e523a49ea1e1
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-06-11 16:58:34 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.java
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.java
@@ -1,17 +1,26 @@
-import java.util.*;
-import java.io.*;
-public class Main {
-    public static void main (String[] args) throws IOException {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st;
-        String S;
-        while((S = br.readLine()) != null) {
-            st = new StringTokenizer(S);
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            int max = Math.max((B-A), (C-B));
-            System.out.println(max-1);
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str);
+
+            int kang1 = Integer.parseInt(st.nextToken());
+            int kang2 = Integer.parseInt(st.nextToken());
+            int kang3 = Integer.parseInt(st.nextToken());
+
+            int answer = Math.max((kang2 - kang1), (kang3 - kang2));
+            answer -= 1;
+
+            System.out.println(answer);
         }
     }
 }
