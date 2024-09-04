61ce5c51b1bbe0855599b8b1f355b8cf718ce457
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-09-04 21:11:48 +0900

    [Bronze V] Title: The World Responds, Time: 92 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/23234. The World Responds/The World Responds.java
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        // 바로 출력하기
         System.out.println("The world says hello!");
     }
 }
