031af4031b490780b832eb6db1b6e1fd45d86b94
eunah0507 <eunah0507@naver.com>
2024-09-14 12:54:23 +0900

    백준 다시 풀기

---
+++ 인프런 알고리즘/java_02.java
@@ -0,0 +1,41 @@
+package BASIC;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class java_02 {
+    public static void main(String[] args) throws Exception {
+
+        // 디큐 사용하기
+        // 제일 위에 있는 카드를 바닥에 버린다. 그 다음,
+        // 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= num; i++) {
+            deque.add(i);
+        }
+
+        while(true){
+            int number = 0;
+
+            if (deque.size() == 1){
+                sb.append(deque.poll());
+                break;
+            }else{
+                deque.pollFirst();
+            }
+
+            number = deque.pollFirst();
+            deque.offerLast(number);
+        }
+
+        System.out.println(sb.toString());
+    }
+}
