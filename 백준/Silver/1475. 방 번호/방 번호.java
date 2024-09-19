7cc0924b9bf1431e5bfcd4dde7fc043d44d1b6fc
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-09-20 00:35:35 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+
+        int[] arr = new int[10];
+        
+        for (int i = 0; i < str.length(); i++) {
+            int num = Character.getNumericValue(str.charAt(i));
+            if (num == 6) {
+                arr[9]++;
+            } else {
+                arr[num]++;
+            }
+        }
+        
+        int max = 0;
+        
+        for (int i = 0; i < 9; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        
+        int nine = arr[9] / 2;
+        
+        if (arr[9] % 2 == 1) {
+            nine++;
+        }
+        
+        max = Math.max(max, nine);
+
+        sb.append(max).append("\n");
+        System.out.println(sb.toString());
+    }
+}
