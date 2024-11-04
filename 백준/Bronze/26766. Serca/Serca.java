b6b2a6b08e36ff83a7d3dee6f38bfcd3eaa22f31
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-11-04 23:48:36 +0900

    [Bronze V] Title: Serca, Time: 304 ms, Memory: 62828 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            sb.append(" @@@   @@@ ").append("\n");
+            sb.append("@   @ @   @").append("\n");
+            sb.append("@    @    @").append("\n");
+            sb.append("@         @").append("\n");
+            sb.append(" @       @ ").append("\n");
+            sb.append("  @     @  ").append("\n");
+            sb.append("   @   @   ").append("\n");
+            sb.append("    @ @    ").append("\n");
+            sb.append("     @     ").append("\n");
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
