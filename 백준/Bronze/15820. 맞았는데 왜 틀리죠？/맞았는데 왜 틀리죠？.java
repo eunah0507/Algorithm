c0551f3e35b8364f6bc4a3486e3b8e0d31fd5649
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-11-14 23:14:56 +0900

    [Bronze III] Title: 맞았는데 왜 틀리죠?, Time: 108 ms, Memory: 14244 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15820. 맞았는데 왜 틀리죠？/맞았는데 왜 틀리죠？.java
+++ 백준/Bronze/15820. 맞았는데 왜 틀리죠？/맞았는데 왜 틀리죠？.java
@@ -6,48 +6,44 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        // 첫 째 줄에 테스트케이스 S1과 시스템 케이스 S2
-        // 둘 째 줄부터는 테스트케이스의 정답과 시스템 케이스의 정답이 주어진다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int testCase = Integer.parseInt(st.nextToken());
         int systemCase = Integer.parseInt(st.nextToken());
-        int count = 0;
-        String answer = "";
 
+        boolean wrongAnswer = false;
+        boolean whyWrong = false;
+
+        // 샘플 테스트케이스 처리
         for (int i = 0; i < testCase; i++) {
             st = new StringTokenizer(br.readLine());
-            int min = Integer.parseInt(st.nextToken());
-            int correct = Integer.parseInt(st.nextToken());
+            int expected = Integer.parseInt(st.nextToken());
+            int actual = Integer.parseInt(st.nextToken());
 
-            if (min == correct) {
-                count++;
-            } else {
-                answer = "Wrong Answer";
+            if (expected != actual) {
+                wrongAnswer = true;
             }
         }
 
+        // 시스템 테스트케이스 처리
         for (int i = 0; i < systemCase; i++) {
             st = new StringTokenizer(br.readLine());
-            int min = Integer.parseInt(st.nextToken());
-            int correct = Integer.parseInt(st.nextToken());
+            int expected = Integer.parseInt(st.nextToken());
+            int actual = Integer.parseInt(st.nextToken());
 
-            if (min == correct) {
-                count++;
-            } else {
-                answer = "Why Wrong!!!";
+            if (expected != actual) {
+                whyWrong = true;
             }
         }
 
-        if (count == (testCase + systemCase)) {
-            answer = "Accepted";
+        // 결과 출력
+        if (wrongAnswer) {
+            System.out.println("Wrong Answer");
+        } else if (whyWrong) {
+            System.out.println("Why Wrong!!!");
+        } else {
+            System.out.println("Accepted");
         }
-
-        sb.append(answer);
-        System.out.println(sb.toString());
-
-
     }
 }
