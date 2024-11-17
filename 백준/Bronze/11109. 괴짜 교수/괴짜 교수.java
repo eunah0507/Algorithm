48b10e17055bf549eedbdf25532e26c89d2b3389
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-11-17 20:40:24 +0900

    [Bronze III] Title: 괴짜 교수, Time: 120 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/11109. 괴짜 교수/괴짜 교수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			
+			if((n * s) < ((n * p) + d)) sb.append("do not parallelize").append("\n");
+			else if((n * s) == ((n * p) + d)) sb.append("does not matter").append("\n");
+			else sb.append("parallelize").append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
