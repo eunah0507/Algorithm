df0644348e95abeb282b163e0213ec41d95aa9c7
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-12-07 23:24:21 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+
+        long answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += (A[i] * B[i]);
+        }
+        System.out.println(answer);
+    }
+}
