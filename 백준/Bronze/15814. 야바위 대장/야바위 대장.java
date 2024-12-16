3b3d304002562131681fef00a7854440a154610e
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-12-16 21:52:40 +0900

    [Bronze II] Title: 야바위 대장, Time: 104 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Bronze/15814. 야바위 대장/야바위 대장.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // T번동안 문자열 S의 A번째 위치에 있는 문자와 B번째 위치에 있는 문자를
+        // 바꾼 결과를 출력하라
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        String [] line = str.split("");
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int numA = Integer.parseInt(st.nextToken());
+            int numB = Integer.parseInt(st.nextToken());
+
+            String tmp = "";
+
+            for (int j = 0; j < str.length(); j++) {
+                if (j == numA){
+                    tmp = line[numA];
+                    line[numA] = line[numB];
+                    line[numB] = tmp;
+                }
+            }
+        }
+
+        for (String s : line) {
+            sb.append(s);
+        }
+
+        System.out.println(sb.toString());
+    }
+}
