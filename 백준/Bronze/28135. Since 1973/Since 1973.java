685761d10258528fd604e3d78bd98d09641675df
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2024-12-20 23:35:29 +0900

    [Bronze III] Title: Since 1973, Time: 208 ms, Memory: 46616 KB -BaekjoonHub

---
+++ 백준/Bronze/28135. Since 1973/Since 1973.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int answer=0;
+        for(int i=1; i<=n; i++){
+            if(String.valueOf(i).contains("50")){
+                answer+=2;
+            }
+            else{
+                answer+=1;
+            }
+        }
+        answer= String.valueOf(n).contains("50") ? answer-=1 : answer;
+        System.out.println(answer);
+
+
+    }
+}
