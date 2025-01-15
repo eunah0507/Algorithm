e7bb7d29e69522e5e2be41ee55f1142e4c3212a6
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-01-16 00:10:05 +0900

    [Bronze II] Title: 대출 요청, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/16497. 대출 요청/대출 요청.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // (대출일, 반납일) 구조
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int [] arr = new int[32];
+        int answer = 0;
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken()) - 1;
+
+            for (int j = start; j <= end; j++) {
+                arr[j]++;
+            }
+        }
+
+        Arrays.sort(arr);
+
+        int book = Integer.parseInt(br.readLine());
+        int maxLoan = 0;
+
+        for (int i = 1; i <= 31; i++) {
+            if (arr[i] > maxLoan){
+                maxLoan = arr[i];
+            }
+        }
+
+        if (maxLoan <= book){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+
+        sb.append(answer);
+        System.out.println(sb.toString());
+    }
+}
