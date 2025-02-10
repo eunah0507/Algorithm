d35807322bed03c9e1f8648664fc49acb4201965
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-02-10 23:50:12 +0900

    [Bronze II] Title: 줄임말 만들기, Time: 128 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.java
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.java
@@ -1,5 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
@@ -7,33 +10,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        // 1. 단어의 앞글자를 따서 만들어야한다.
-        // 2. 'i', 'pa', 'te', 'ni', 'niti', 'a', 'ali', 'nego', 'no', 'ili'. 무시한다.
-        // 3. 2번의 단어들이 맨 앞에 있으면 무시X
-
-        // 1, spilt을 이용하여 배열에 가둔다.
-        // 2. 단 arr[0]이 2번일 경우에는 포함한다.
-        // 3. 2번일 경우 무시한다.
-        // 4. 각 배열의 순서에 맨 앞 글자를 answer에 더한다.
-
         String str = br.readLine();
+        Set<String> mz = new HashSet<>(Arrays.asList("i", "pa", "te",
+                "ni", "niti", "a", "ali",  "nego", "no", "ili"));
+
         String[] arr = str.split(" ");
         String answer = "";
 
         for (int i = 0; i < arr.length; i++) {
             char ch = arr[i].charAt(0);
 
-            if (i == 0) {
-                answer += ch;
+            if (i != 0 && mz.contains(arr[i])) {
+                continue;
             }else {
-                if (arr[i].equals("i") || arr[i].equals("pa") || arr[i].equals("te") ||
-                        arr[i].equals("ni") || arr[i].equals("niti") || arr[i].equals("a") ||
-                        arr[i].equals("ali") || arr[i].equals("nego") || arr[i].equals("no") ||
-                        arr[i].equals("ili")){
-                    continue;
-                }else{
-                    answer += ch;
-                }
+               answer += ch;
             }
         }
 
