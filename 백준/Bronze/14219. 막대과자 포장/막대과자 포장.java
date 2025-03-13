35b8537fcd9ce0b59fef4cf89d4fe25749e8ced9
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-03-13 23:42:52 +0900

    [Bronze II] Title: 막대과자 포장, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/14219. 막대과자 포장/막대과자 포장.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append((n * m) % 3 == 0 ? "YES" : "NO");
+        
+        System.out.println(sb);
+    }
+}
