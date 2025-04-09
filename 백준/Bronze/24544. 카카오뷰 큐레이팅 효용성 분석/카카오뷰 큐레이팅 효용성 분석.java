d5a55f915c17eca44df3abaed1634f13aa5c59a7
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-04-09 23:40:43 +0900

    [Bronze II] Title: 카카오뷰 큐레이팅 효용성 분석, Time: 108 ms, Memory: 14836 KB -BaekjoonHub

---
+++ 백준/Bronze/24544. 카카오뷰 큐레이팅 효용성 분석/카카오뷰 큐레이팅 효용성 분석.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+
+		String[] aTokens = br.readLine().split(" ");
+		String[] bTokens = br.readLine().split(" ");
+
+		int total = 0;
+		int unregistered = 0;
+
+		for (int i = 0; i < n; i++) {
+			int interest = Integer.parseInt(aTokens[i]);
+			int isRegistered = Integer.parseInt(bTokens[i]);
+
+			total += interest;
+
+			if (isRegistered == 0) {
+				unregistered += interest;
+			}
+		}
+
+		sb.append(total).append('\n').append(unregistered);
+		System.out.println(sb.toString());
+	}
+}
