81c72f90df95d3a386d06980b2d4088d48424042
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-06-10 23:43:12 +0900

    [Bronze III] Title: 3대 512, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/33990. 3대 512/3대 512.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(br.readLine());
+        int answer = -1;
+        
+        for (int i = 0; i < num; i++) {
+            String[] input = br.readLine().split(" ");
+            int sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]) + Integer.parseInt(input[2]);
+            
+            if (sum >= 512) {
+                if (answer == -1 || sum < answer) {
+                    answer = sum;
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer);
+        
+        System.out.println(sb.toString());
+    }
+}
