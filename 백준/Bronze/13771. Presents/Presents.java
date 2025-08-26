4fc6693af020c1964f4b7a405c87cab001fca486
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-08-26 21:47:54 +0900

    [Bronze II] Title: Presents, Time: 108 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Bronze/13771. Presents/Presents.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Double.parseDouble(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        sb.append(String.format("%.2f", arr[1]));
+        System.out.println(sb.toString());
+    }
+}
