75826749cf073c9f915f819c001fe940b1018c1e
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-11-28 20:03:46 +0900

    [Silver I] Title: 포도주 시식, Time: 116 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,25 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
+
         int n = Integer.parseInt(br.readLine());
-        int[] w = new int[n + 1];
         
-        for (int i = 1; i <= n; i++) w[i] = Integer.parseInt(br.readLine());
+        int[] wine = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        // n이 1, 2일 때는 바로 처리
+        if (n == 1) {
+            sb.append(wine[1]);
+            System.out.println(sb);
+            return;
+        }
+        if (n == 2) {
+            sb.append(wine[1] + wine[2]);
+            System.out.println(sb);
+            return;
+        }
+
         int[] dp = new int[n + 1];
-        
-        if (n >= 1) dp[1] = w[1];
-        if (n >= 2) dp[2] = w[1] + w[2];
-        
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
         for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + w[i], dp[i - 3] + w[i - 1] + w[i]));
+            int case1 = dp[i - 1];                     
+            int case2 = dp[i - 2] + wine[i];           
+            int case3 = dp[i - 3] + wine[i - 1] + wine[i];  
+            dp[i] = Math.max(case1, Math.max(case2, case3));
         }
 
         sb.append(dp[n]);
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
