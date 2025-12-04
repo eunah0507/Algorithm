469ef2fecd9640343b0046a5f68354cdf277b4cc
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2025-12-04 23:27:42 +0900

    [Bronze I] Title: 코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/34760. 코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데/코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데.java
+++ 백준/Bronze/34760. 코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데/코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         long maxFirst = 0L;
@@ -25,6 +24,7 @@ public class Main {
         }
 
         long ans = maxFirst + 1;
+        
         if (ans < last) {
             ans = last;
         }
