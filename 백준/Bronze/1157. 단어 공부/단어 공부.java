b2be9dd3ed2c7bc22e18f17a0041cdcc0d9a3d95
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-01-27 23:50:33 +0900

    [Bronze I] Title: 단어 공부, Time: 232 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,41 +1,46 @@
-import java.util.Iterator;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		// 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 문제
 
-		// 1. 테스트케이스 String을 받아온다.
-		// 2. toUppercase()를 통해 모두 대문자화한다.
-		// 3. charAt()을 써서 한글자씩 뽑아낸다.
-		// 4. 알파벳 개수만큼인 26개의 배열을 만든다.
-		// 4. for문을 str.length()만큼 돌려서 해당 알파벳이 들어있을 때 마다 배열에 +1해준다.
-		// 5. for문을 돌려서 알파벳 개수 배열중 가장 큰 값을 출력한다.
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		Scanner sc = new Scanner(System.in);
+        String input = br.readLine();
+        int[] count = new int[26];
 
-		String str = sc.next();
-		str = str.toUpperCase();
-		int[] arr = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
 
-		for (int i = 0; i < str.length(); i++) {// EWR
-			char c = str.charAt(i); // A~Z
-			int abc = c - 'A';
-			arr[abc]++;
-		}
+            if (c >= 'a' && c <= 'z') {
+                count[c - 'a']++;
+            } else {
+                count[c - 'A']++;
+            }
+        }
 
-		int max = Integer.MIN_VALUE;
-		char change = ' ';
+        int max = 0;
+        int index = -1;
+        boolean duplicate = false;
 
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-				change = (char)('A' + i); //강제 형변환 , 알파벳 위치를 반환해주는 것.
-			}else if(arr[i] == max) {
-				max = arr[i];
-				change = '?';
-			}
-		}
-		System.out.println(change);
-	}
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                index = i;
+                duplicate = false;
+            } else if (count[i] == max) {
+                duplicate = true;
+            }
+        }
+
+        if (duplicate) {
+            sb.append("?");
+        } else {
+            sb.append((char) ('A' + index));
+        }
+
+        System.out.print(sb.toString());
+    }
 }
