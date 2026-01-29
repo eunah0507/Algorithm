dd60a680523a8e33f8b32b685c25c5feec36317d
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-01-29 23:28:16 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 84 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        int count = 0;
+
+        while(str != null){
+            count++;
+            str = br.readLine();
+        }
+
+        sb.append(count);
+        System.out.println(sb.toString());
+
+    }
+}
