211a1827d9c4c0cbffa0595ba28193624940f745
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-03-09 23:11:34 +0900

    [Silver V] Title: Banana, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/18679. Banana/Banana.java
+++ 백준/Silver/18679. Banana/Banana.java
@@ -6,10 +6,12 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        
         Map<String, String> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
