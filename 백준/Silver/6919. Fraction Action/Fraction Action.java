d75305c8d3d4b78e4dc40b2f15a940415aca5df5
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-03-13 23:15:04 +0900

    [Silver V] Title: Fraction Action, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/6919. Fraction Action/Fraction Action.java
+++ 백준/Silver/6919. Fraction Action/Fraction Action.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
     static long gcd(long a, long b) {
         while (b != 0) {
             long t = a % b;
@@ -13,6 +12,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
