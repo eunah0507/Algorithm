7ca0cfc91ac5fe42ccc980774262a9b95bc68c4f
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-04-23 21:35:13 +0900

    [Gold IV] Title: 플로이드, Time: 432 ms, Memory: 52824 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] dist = new int[n + 1][n + 1];
+        int inf = 1000000000;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = inf;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int c = Integer.parseInt(input[2]);
+
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] == inf || dist[k][j] == inf) {
+                        continue;
+                    }
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == inf) {
+                    sb.append(0);
+                } else {
+                    sb.append(dist[i][j]);
+                }
+
+                if (j < n) {
+                    sb.append(' ');
+                }
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
