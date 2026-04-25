23d352c7821b582bd59385091bdc1cb431a8f6fd
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-04-25 21:12:21 +0900

    [Gold V] Title: 회의실 배정, Time: 488 ms, Memory: 44296 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] meetings = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            meetings[i][0] = Integer.parseInt(st.nextToken());
+            meetings[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+
+        int count = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (meetings[i][0] >= endTime) {
+                count++;
+                endTime = meetings[i][1];
+            }
+        }
+
+        sb.append(count);
+        System.out.print(sb.toString());
+    }
+}
