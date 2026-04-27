88f9f1105a041fe2548291627ea4e992f3a4e14f
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-04-27 22:31:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 112 ms, Memory: 15684 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int[] arr;
+    static int[] op = new int[4];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 4; i++) {
+            op[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(1, arr[0]);
+
+        sb.append(max).append('\n').append(min);
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int depth, int value) {
+        if (depth == n) {
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (op[i] > 0) {
+                op[i]--;
+
+                if (i == 0) {
+                    dfs(depth + 1, value + arr[depth]);
+                } else if (i == 1) {
+                    dfs(depth + 1, value - arr[depth]);
+                } else if (i == 2) {
+                    dfs(depth + 1, value * arr[depth]);
+                } else {
+                    dfs(depth + 1, value / arr[depth]);
+                }
+
+                op[i]++;
+            }
+        }
+    }
+}
