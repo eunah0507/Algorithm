651870194b43c8a280c97b45183cfb19679bfb55
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-04-27 22:27:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 18976 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[k + 1];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            for (int j = k; j >= w; j--) {
+                if (dp[j] < dp[j - w] + v) {
+                    dp[j] = dp[j - w] + v;
+                }
+            }
+        }
+
+        sb.append(dp[k]);
+
+        System.out.print(sb.toString());
+    }
+}
